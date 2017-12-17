aa23aa7d7e5e647996b6314f4e31df8abf7c7c96
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2017-12-17 11:22:34 -0700

    Initial commit

--- scripts/LinkedList.js
+++ scripts/LinkedList.js
@@ -2,6 +2,72 @@ import Node from './Node';
 
 export default class LinkedList {
   constructor () {
+    this.length = 0;
+    this.head = null;
+    this.pushCount = 0;
+  }
 
+  unshift(data) {
+    this.length++;
+    this.next = this.head;
+    this.head = new Node(data, this.next);
+  }
+
+  shift() {
+    if(this.length === 0) {
+      return null;
+    }
+
+    let shifted = this.head;
+    this.head = this.next;
+    this.length--;
+
+    if(this.length === 0){
+      this.head = null;
+    }
+
+    return shifted;
+  }
+
+  push(data) {
+    if(this.length === 0) {
+      this.head = new Node(data, this.next);
+    } else if(this.length === 1) {
+      this.head.next = new Node(data, this.next);
+    } else if(this.length === 2) {
+      this.head.next.next = new Node(data, this.next);
+    } else if (this.length === 3) {
+      this.head.next.next.next = new Node(data, this.next);
+    }
+    
+    this.length++;
+  }
+
+  pop() {
+    if(this.length === 0) {
+      return null;
+    } 
+
+    if(this.length === 1) {
+      let popped = this.head;
+      this.head = null;
+      this.length--;
+      return popped;
+    } else if(this.length === 2) {
+      let popped = this.head.next;
+      this.head.next = null;
+      this.length--;
+      return popped;
+    } else if(this.length === 3) {
+      let popped = this.head.next.next;
+      this.head.next.next = null;
+      this.length--;
+      return popped;
+    } else if(this.length === 4) {
+      let popped = this.head.next.next.next;
+      this.head.next.next.next.next = null;
+      this.length--;
+      return popped;
+    }
   }
 }
